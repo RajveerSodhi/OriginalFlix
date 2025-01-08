80a0f4fb49de30ff7301f5568f8d462f0c7a6865
RajveerSodhi <rajveersodhi03@gmail.com>
2025-01-07 23:57:24 -0800

    fix database url

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,12 +1,12 @@
 from os import environ as env
-from os import getenv
-from dotenv import load_dotenv
+# from os import getenv
+# from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_POSTGRES_PASSWORD')}@{getenv('LOCAL_POSTGRES_HOST')}:{getenv('LOCAL_POSTGRES_PORT')}/{getenv('LOCAL_POSTGRES_DB')}"
+# DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_POSTGRES_PASSWORD')}@{getenv('LOCAL_POSTGRES_HOST')}:{getenv('LOCAL_POSTGRES_PORT')}/{getenv('LOCAL_POSTGRES_DB')}"
 # DATABASE_URL = f"postgresql://{env['LOCAL_POSTGRES_ADMIN']}:{env['LOCAL_POSTGRES_PASSWORD']}@{env['LOCAL_POSTGRES_HOST']}:{env['LOCAL_POSTGRES_PORT']}/{env['LOCAL_POSTGRES_DB']}"
 
 # DATABASE_URL = (
@@ -14,10 +14,10 @@ DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_PO
 #     f"@{getenv('AZURE_POSTGRES_HOST')}:{getenv('AZURE_POSTGRES_PORT')}/{getenv('AZURE_POSTGRES_DB')}?sslmode=require"
 # )
 
-# DATABASE_URL = (
-#     f"postgresql://{env['AZURE_POSTGRES_ADMIN']}:{env['AZURE_POSTGRES_PASSWORD']}"
-#     f"@{env['AZURE_POSTGRES_HOST']}:{env['AZURE_POSTGRES_PORT']}/{env['AZURE_POSTGRES_DB']}?sslmode=require"
-# )
+DATABASE_URL = (
+    f"postgresql://{env['AZURE_POSTGRES_ADMIN']}:{env['AZURE_POSTGRES_PASSWORD']}"
+    f"@{env['AZURE_POSTGRES_HOST']}:{env['AZURE_POSTGRES_PORT']}/{env['AZURE_POSTGRES_DB']}?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
