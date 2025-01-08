f68c1d4eaee1339026eb0f1da67cf7afa1a3f6c3
RajveerSodhi <rajveersodhi03@gmail.com>
2025-01-07 22:17:30 -0800

    database env updates

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,13 @@
-from os import getenv
-from dotenv import load_dotenv
+from os import environ as env
+# from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_POSTGRES_PASSWORD')}@{getenv('LOCAL_POSTGRES_HOST')}:{getenv('LOCAL_POSTGRES_PORT')}/{getenv('LOCAL_POSTGRES_DB')}"
+# DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_POSTGRES_PASSWORD')}@{getenv('LOCAL_POSTGRES_HOST')}:{getenv('LOCAL_POSTGRES_PORT')}/{getenv('LOCAL_POSTGRES_DB')}"
+DATABASE_URL = f"postgresql://{env['LOCAL_POSTGRES_ADMIN']}:{env['LOCAL_POSTGRES_PASSWORD']}@{env['LOCAL_POSTGRES_HOST']}:{env['LOCAL_POSTGRES_PORT']}/{env['LOCAL_POSTGRES_DB']}"
+
 # DATABASE_URL = (
 #     f"postgresql://{getenv('AZURE_POSTGRES_ADMIN')}:{getenv('AZURE_POSTGRES_PASSWORD')}"
 #     f"@{getenv('AZURE_POSTGRES_HOST')}:{getenv('AZURE_POSTGRES_PORT')}/{getenv('AZURE_POSTGRES_DB')}?sslmode=require"
