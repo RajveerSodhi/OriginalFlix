e92ee7a11ef680fb05e7ecfe3e36a2b051ef0861
RajveerSodhi <rajveersodhi03@gmail.com>
2025-01-07 22:20:24 -0800

    updated DB connection string

--- backend/database.py
+++ backend/database.py
@@ -6,12 +6,17 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # load_dotenv()
 
 # DATABASE_URL = f"postgresql://{getenv('LOCAL_POSTGRES_ADMIN')}:{getenv('LOCAL_POSTGRES_PASSWORD')}@{getenv('LOCAL_POSTGRES_HOST')}:{getenv('LOCAL_POSTGRES_PORT')}/{getenv('LOCAL_POSTGRES_DB')}"
-DATABASE_URL = f"postgresql://{env['LOCAL_POSTGRES_ADMIN']}:{env['LOCAL_POSTGRES_PASSWORD']}@{env['LOCAL_POSTGRES_HOST']}:{env['LOCAL_POSTGRES_PORT']}/{env['LOCAL_POSTGRES_DB']}"
+# DATABASE_URL = f"postgresql://{env['LOCAL_POSTGRES_ADMIN']}:{env['LOCAL_POSTGRES_PASSWORD']}@{env['LOCAL_POSTGRES_HOST']}:{env['LOCAL_POSTGRES_PORT']}/{env['LOCAL_POSTGRES_DB']}"
 
 # DATABASE_URL = (
 #     f"postgresql://{getenv('AZURE_POSTGRES_ADMIN')}:{getenv('AZURE_POSTGRES_PASSWORD')}"
 #     f"@{getenv('AZURE_POSTGRES_HOST')}:{getenv('AZURE_POSTGRES_PORT')}/{getenv('AZURE_POSTGRES_DB')}?sslmode=require"
 # )
+
+DATABASE_URL = (
+    f"postgresql://{env['AZURE_POSTGRES_ADMIN']}:{env['AZURE_POSTGRES_PASSWORD']}"
+    f"@{env['AZURE_POSTGRES_HOST']}:{env['AZURE_POSTGRES_PORT']}/{env['AZURE_POSTGRES_DB']}?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
