83526c7e751371666e4806b5bb648624d06e56dc
RajveerSodhi <rajveersodhi03@gmail.com>
2025-01-09 02:58:24 -0800

    trial 3

--- backend/database.py
+++ backend/database.py
@@ -19,7 +19,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 #     f"@{env['AZURE_POSTGRES_HOST']}:{env['AZURE_POSTGRES_PORT']}/{env['AZURE_POSTGRES_DB']}?sslmode=require"
 # )
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5433/originalflix"
+# DATABASE_URL = "postgresql://postgres:root@localhost:5433/originalflix"
+DATABASE_URL = "postgresql://rajveersodhi:Postgres!@originalflix.postgres.database.azure.com:5432/postgres?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
