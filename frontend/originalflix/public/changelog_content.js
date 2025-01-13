8ee38f08a082b5af536139c467964ad2700608fd
RajveerSodhi <rajveersodhi03@gmail.com>
2025-01-13 02:23:18 -0800

    added changelog content

---
+++ frontend/originalflix/public/changelog_content.js
@@ -0,0 +1,9 @@
+export const changelog = [
+    {
+        date: "Jan 18, 2025",
+        content: [
+            "Due to backend errors, data for Netflix Original movies from 2020-2023 and all of Zee5's original movie catalog is not included as of now.",
+            "OriginalFlix API v1 deployed."
+        ]
+    },
+]
