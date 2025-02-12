7a001cd76446fc041b25a602ce113fa99695b25d
RajveerSodhi <rajveersodhi03@gmail.com>
2025-02-12 11:23:58 -0800

    layout bug fix

--- frontend/originalflix/src/components/header.js
+++ frontend/originalflix/src/components/header.js
@@ -2,7 +2,7 @@ import DocsButton from "./docsButton";
 
 function Header() {
     return (
-        <section className="flex flex-row w-screen">
+        <section className="flex flex-row w-full">
             <div className="text-[4.5rem] w-full flex items-start flex-col text-left justify-center pl-8 pr-8 sm:pr-0">
             <strong className="mb-8  leading-[5.5rem]">The <em>EXACT</em> database you need.</strong>
             <DocsButton />
